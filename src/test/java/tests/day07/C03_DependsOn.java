8f8b09491df9659be422850c2b6aaf7ae126ab99
zynpblt <fzaydin6636@gmail.com>
2021-10-11 21:20:22 +0100

    TestNG

---
+++ src/test/java/tests/day07/C03_DependsOn.java
@@ -0,0 +1,11 @@
+package tests.day07;
+
+public class C03_DependsOn {
+    // Bir class oluşturun: DependsOnTest
+    //● https://www.amazon.com/ adresine gidin.
+    //	1. Test : Amazon ana sayfaya gittiginizi test edin
+    //	2. Test : 1.Test basarili ise search Box’i kullanarak “Nutella” icin arama yapin ve aramanizin gerceklestigini Test edin
+    //	3.Test : “Nutella” icin arama yapildiysa ilk urunu tiklayin ve fiyatinin 	$16.83 oldugunu test edin
+
+
+}
